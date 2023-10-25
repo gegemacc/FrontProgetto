6073fdc9e75784b23014833735825b0172819e92
AleMendy <alessandro_01@live.it>
2023-10-25 15:50:44 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SignupComponent} from "./pages/signup/signup.component";
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
     path: 'register',
     component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
